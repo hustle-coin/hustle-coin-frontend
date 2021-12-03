6812a7213df2cb213a64593a92ee86b7197df632
Jonathan Adams <60414498+jonathan-adams-fullstack@users.noreply.github.com>
2021-12-03 02:27:08 -0500

    Update index.js

--- gatsby-landing/src/sections/Subscribe/index.js
+++ gatsby-landing/src/sections/Subscribe/index.js
@@ -11,17 +11,6 @@ import { SectionTitle } from "reusecore/SectionTitle"
 
 import SubscribeWrapper from "./subscribe.style"
 
-import bscscan, { account } from "bsc-scan";
-
-bscscan.setUrl("https://testnet.bscscan.com");
-bscscan.setApiKey("DQKEFS56AKQ1NXY3KVIZWK2UFG8IP5RY46");
-
-const start = async () => {
-  try {
-    const balance = await account.getTransactions(
-      "0xc3266525723960c69263a4b1b6f12042a37cb6addc022a8e20651639b43c8d17"
-    );
-
 const Subscribe = () => {
   return (
     <SubscribeWrapper id="contact">
