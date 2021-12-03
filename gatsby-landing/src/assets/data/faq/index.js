3dbc93211cb2a00ae39856b869df045878ed5a6c
Jonathan Adams <60414498+jonathan-adams-fullstack@users.noreply.github.com>
2021-12-03 12:48:22 -0500

    Update index.js

--- gatsby-landing/src/assets/data/faq/index.js
+++ gatsby-landing/src/assets/data/faq/index.js
@@ -2,40 +2,39 @@
 export default {
   faq: [
     {
-        name: "Pre-ICO & ICO",
+        name: "Pre-Sale",
         items: [
             {
                 title: "What is ICO Crypto?",
                 body: "An initial coin offering or initial currency offering is a type of funding using cryptocurrencies. It is often a form of crowdfunding, although a private ICO which does not seek public investment is also possible."
             },
             {
-                title: "What cryptocurrencies can I use to purchase?",
-                body: "Tokens can be swapped using BNB"
+                title: "What cryptocurrencies can I use to buy Hustle Coin?",
+                body: "HSTL can be swapped with BNB"
             },
             {
-                title: "How can I participate in the ICO Token sale?",
+                title: "Where can I buy Hustle Coin Pre-Sale Tokens?",
                 body: "Tokens can only be bought during our Pre-Sale through www.pinksale.finance"
             },
             {
-                title: "How do I benefit from the ICO Token?",
+                title: "How do I benefit from buying HSTL during the pre-sale?",
                 body: "During our Pre-Sale of Huslte Coin you get an additional 25% in HSTL tokens."
             },
         ]
     },
     {
-        name: "General",
+        name: "Moon Wallet",
         items: [
             {
-                title: "How can I participate in the ICO Token sale?",
+                title: "What is the Moon Wallet?",
                 body: "Cryptik - is unique platform; that is secure, smart and easy-to-use platform, and completely disrupting the way businesses raise capital. Below we’ve provided a bit of ICO, ICO Token, cryptocurrencies, and few others."
             },
-        ]
-    },
-    {
-        name: "Tokens",
-        items: [
             {
-                title: "How can I participate in the ICO Token sale?",
+                title: "Who can win the Moon Wallet?",
+                body: "Cryptik - is unique platform; that is secure, smart and easy-to-use platform, and completely disrupting the way businesses raise capital. Below we’ve provided a bit of ICO, ICO Token, cryptocurrencies, and few others."
+            },
+            {
+                title: "Can I donate to the Moon Wallet?",
                 body: "Cryptik - is unique platform; that is secure, smart and easy-to-use platform, and completely disrupting the way businesses raise capital. Below we’ve provided a bit of ICO, ICO Token, cryptocurrencies, and few others."
             },
         ]
