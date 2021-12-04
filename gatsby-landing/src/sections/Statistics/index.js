1c793224e92c010389541891de4501887f8e1be7
Jonathan Adams <60414498+jonathan-adams-fullstack@users.noreply.github.com>
2021-12-04 02:57:42 -0500

    Update index.js

--- gatsby-landing/src/sections/Statistics/index.js
+++ gatsby-landing/src/sections/Statistics/index.js
@@ -15,16 +15,81 @@ const Statistics = () => {
       <Box className="statistics-wrapper">
         <Container>
           <Row>
-            <Col className="md-6 sm-6">
+            <Col className="md-6 sm-6" style={{marginLeft: "auto", marginRight: "auto"}} >
               <Box className="statistics-image">
-                <Heading as="h2">Pre-Sale Allocation</Heading>
-                <img src={statisticsImage1} alt="cryptic statistics image" />
+                <Heading as="h2">Funds Allocation</Heading>
+                <div style={{ marginLeft: "auto", marginRight: "auto",marginTop:"80px", width: "240px", height: "400px" }}>
+                  <div class="pie p10" ></div>
+                  <div class="pie p30" ></div>
+                  <div class="pie p60-2" ></div>
+                  <div class="pie p60"></div>
+                  <div class="pie center" ></div>
+                  <div className="legend l60" >
+                    <div className="label" style={{paddingInline:"10px"}}>Liquidity</div>
+                    <hr className="line"/>
+                    <div class="point" ></div>
+                    <div style={{  width: "15px", height: "15px", marginTop:"10px", color:"white", fontSize:"15px"  }}>60%</div>
+                  </div>
+                  <div className="legend l10" >
+                    <div className="label" style={{transform: "rotate(-45deg)", paddingInline:"5px"}}>Marketing</div>
+                    <hr className="line"/>
+                    <div class="point" ></div>
+                    <div style={{ transform: "rotate(-45deg)", width: "15px", height: "15px", marginTop:"10px", color:"white", fontSize:"15px"  }}>10%</div>
+                  </div>
+                  <div className="legend l30" >
+                    <div className="label" style={{paddingInline:"5px"}}>Moon Wallet</div>
+                    <hr className="line"/>
+                    <div class="point" ></div>
+                    <div style={{ width: "15px", height: "15px", marginTop:"10px", marginRight:"10%", color:"white", fontSize:"15px"  }}>30%</div>
+                  </div>
+                </div>
+
+
+                {/* <img src={statisticsImage1} alt="cryptic statistics image" /> */}
               </Box>
             </Col>
-            <Col className="md-6 sm-6">
+            <Col className="md-6 sm-6" style={{marginLeft: "auto", marginRight: "auto"}}>
               <Box className="statistics-image">
                 <Heading as="h2">Token Distribution</Heading>
-                <img src={statisticsImage2} alt="cryptic statistics image" />
+
+                <div style={{ marginLeft: "auto", marginRight: "auto",marginTop:"80px", width: "240px", height: "400px" }}>
+                  <div class="pie p6-5" ></div>
+                  <div class="pie p6" ></div>
+                  <div class="pie p25" ></div>
+                  <div class="pie p62"></div>
+                  <div class="pie p60-2" ></div>
+                  <div class="pie center" ></div>
+                  <div className="legend l60">
+                    <div style={{paddingInline:"10px"}}>Locked</div>
+                    <hr className="line"/>
+                    <div class="point" ></div>
+                    <div style={{ width: "15px", height: "15px", marginTop:"20px", color:"white", fontSize:"15px"  }}>62.5%</div>
+                  </div>
+
+                  <div className="legend l30">
+                    <div style={{paddingInline:"10px", width:"95px"}}>Pre-Sale</div>
+                    <hr className="line"/>
+                    <div class="point" ></div>
+                    <div style={{ width: "15px", height: "15px", marginTop:"10px", marginRight:"10%", color:"white", fontSize:"15px"  }}>25%</div>
+                  </div>
+
+                  <div className="legend l6-5" >
+                    <div className="label" style={{transform: "rotate(-45deg)", paddingInline:"2px"}}>Developers</div>
+                    <hr className="line"/>
+                    <div class="point" ></div>
+                    <div style={{ transform: "rotate(-45deg)", width: "10px", height: "10px", marginTop:"5px", color:"white", fontSize:"12px"  }}>6.5%</div>
+                  </div>
+
+                  <div className="legend l6" >
+                    <div className="label" style={{transform: "rotate(-65deg)", paddingInline:"-200px"}}>Marketing</div>
+                    <hr className="line"/>
+                    <div class="point" ></div>
+                    <div style={{ transform: "rotate(-65deg)", width: "10px", height: "10px", marginTop:"5px", color:"white", fontSize:"12px"  }}>6%</div>
+                  </div>
+                </div>
+
+
+                {/* <img src={statisticsImage2} alt="cryptic statistics image" /> */}
               </Box>
             </Col>
           </Row>
