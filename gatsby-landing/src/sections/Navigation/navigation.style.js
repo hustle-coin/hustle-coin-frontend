5e5c1ee2e188591de14b4b9c499461cfaa7d8588
Jonathan Adams <60414498+jonathan-adams-fullstack@users.noreply.github.com>
2021-12-04 16:07:05 -0500

    Update navigation.style.js

--- gatsby-landing/src/sections/Navigation/navigation.style.js
+++ gatsby-landing/src/sections/Navigation/navigation.style.js
@@ -123,7 +123,7 @@ const NavigationWrap =  styled.header`
     margin-top: 8px;
     transition: all 0.8s cubic-bezier(0.3, 0.8, 0.2, 1) 0s;
     img{
-      width: 135px;
+      width: 185px;
     }
   }
   &.scrolled{
