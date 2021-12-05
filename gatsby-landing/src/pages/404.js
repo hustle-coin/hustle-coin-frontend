754cdb29ca0249c089f9c88ef24500765c69988c
Jonathan Adams <60414498+jonathan-adams-fullstack@users.noreply.github.com>
2021-12-04 20:41:52 -0500

    Update 404.js

--- gatsby-landing/src/pages/404.js
+++ gatsby-landing/src/pages/404.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Navigation from "sections/Navigation";
-import BlogItems from "sections/Blog-full";
-import Footer from "sections/Footer";
+import { IoIosArrowRoundForward } from "react-icons/io"
+import Box from "reusecore/Box"
+import Text from "reusecore/Text"
+import Heading from "reusecore/Heading"
+import { Container, Row, Col } from "reusecore/Layout"
 
-import theme from "assets/theme/theme";
-import GlobalStyle from "assets/theme";
+import PageHeader from "../../components/PageHeader"
+
+import data from "assets/data/blog"
+import { BlogPageWrapper } from "./blogFull.style"
 
 const NotFoundPage = () => (
   <ThemeProvider theme={theme}>
