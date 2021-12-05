90f087b28f693d9e0eacccd2843fa1c9c26118c6
Jonathan Adams <60414498+jonathan-adams-fullstack@users.noreply.github.com>
2021-12-04 22:26:43 -0500

    Update 404.js

--- gatsby-landing/src/pages/404.js
+++ gatsby-landing/src/pages/404.js
@@ -1,23 +1,15 @@
-import React from "react";
+import React from "react"
 import { ThemeProvider } from "styled-components"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo";
-import Navigation from "sections/Navigation";
-import Banner from "sections/Banner";
-import Service from "sections/Service";
-import CoinFund from "sections/CoinFund";
-import About from "sections/About";
-import Awards from "sections/tokenomics";
-import Wallet from "sections/Wallet";
-import Statistics from "sections/Statistics";
-import Faq from "sections/Faq";
-import Subscribe from "sections/Subscribe";
-import Stack from "sections/Stack";
-import Footer from "sections/Footer";
+import SEO from "../components/seo"
 
-import theme from "assets/theme/theme";
-import GlobalStyle from "assets/theme";
+import Navigation from "sections/Navigation"
+import BlogSinglePage from "sections/Blog-single"
+import Footer from "sections/Footer"
+
+import theme from "assets/theme/theme"
+import GlobalStyle from "assets/theme"
 
 const NotFoundPage = () => (
   <ThemeProvider theme={theme}>
