a507556b5159eca5c55f9c7e3256b3e93c28941f
Jonathan Adams <60414498+jonathan-adams-fullstack@users.noreply.github.com>
2021-12-04 20:48:03 -0500

    Update 404.js

--- gatsby-landing/src/pages/404.js
+++ gatsby-landing/src/pages/404.js
@@ -1,27 +1,93 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Navigation from "sections/Navigation";
-import BlogItems from "sections/Blog-full";
-import Footer from "sections/Footer";
+import { Container, Row, Col } from "reusecore/Layout"
+import Box from "reusecore/Box"
+import Badge from "reusecore/Badge"
+import Heading from "reusecore/Heading"
+import Text from "reusecore/Text"
 
-import theme from "assets/theme/theme";
-import GlobalStyle from "assets/theme";
+import BannerImage from "assets/images/banners/banner-one/banner-one-thumb.svg"
 
-const NotFoundPage = () => (
-  <ThemeProvider theme={theme}>
-    <Layout>
-      <GlobalStyle />
-      <SEO title="404: Not found" />
+import particleTopLeft from "assets/images/particles/banner/particle-top-left.png"
+import particleUnderLogo from "assets/images/particles/banner/particle-under-logo.png"
+import prticleTopRight from "assets/images/particles/banner/prticle-top-right.png"
+import particleBottomLeft from "assets/images/particles/banner/particle-bottom-left.png"
+import particleBottomRight from "assets/images/particles/banner/particle-bottom-right.png"
 
-      <Navigation />
-      <h1>RUG-PULL.......NOT!!</h1>
-      <p>This is NOT a rug-pull....you just hit a page that doesn&#39;t exist... the sadness.</p>
-      <BlogItems />
-      <Footer/>
-    </Layout>
-  </ThemeProvider>
-)
-export default NotFoundPage
+import BannerWrapper from "./banner.style"
+
+const Banner = () => {
+  return (
+    <BannerWrapper>
+      <img
+        src={particleTopLeft}
+        className="section__particle top-left"
+        alt="cryptik particles"
+      />
+      <img
+        src={particleUnderLogo}
+        className="section__particle two"
+        alt="cryptik particles"
+      />
+      <img
+        src={prticleTopRight}
+        className="section__particle top-right"
+        alt="cryptik particles"
+      />
+      <img
+        src={particleBottomLeft}
+        className="section__particle bottom-left"
+        alt="cryptik particles"
+      />
+      <img
+        src={particleBottomRight}
+        className="section__particle bottom-right"
+        alt="cryptik particles"
+      />
+      <Container>
+        <Row>
+          <Col className="lg-8 xs-12">
+            <Box className="banner-content">
+              <Badge className="offer-text">
+                say hello to the moon
+              </Badge>
+              <Heading as="h1">
+                About Hustle Coin
+              </Heading>
+              <Text>
+                Hustle Coin offers a unique lottery system in which holders of{" "}
+                <br />
+                HSTL have a chance each month at being awarded 100% of the Moon
+                <br /> Wallet balance!
+                </Text>
+                <Text>
+                No additional purchase is needed to be entered in for your chance
+                to win.{" "}
+                <br />
+                Simply hold HSLT and your wallet might be the next to see the Moon!
+              </Text>
+              <Box className="banner-btn">
+                <Link to="#" className="btn btn-fill">
+                  {" "}
+                  Pre-Sale{" "}
+                </Link>
+                <Link to="#" className="btn">
+                  {" "}
+                  white paper{" "}
+                </Link>
+              </Box>
+            </Box>
+          </Col>
+        </Row>
+        <img
+          src={BannerImage}
+          className="banner__thumb"
+          alt="hustle coin banner thumb"
+        />
+      </Container>
+    </BannerWrapper>
+  )
+}
+
+export default Banner
